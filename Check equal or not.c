d03ff1aa40e8bc86add23f2f5fde2083e5cdbaf0
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 11:34:36 +0600

    Add files via upload

---
+++ Check equal or not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+    int num1,num2;
+
+    printf("Enter 1st number :");
+    scanf("%d",&num1);
+    printf("\nEnter 2nd number :");
+    scanf("%d",&num2);
+
+    if(num1==num2)
+        printf("Number are equal");
+    else
+        printf("Number are not equal");
+
+    getch();
+
+}
